244381595e7e8062502596252c1375969d925e27
Martin Wrangenby <martinwrangenby@gmail.com>
2022-10-24 13:50:32 +0200

    Adjust shell script formatting

--- docker-execution.sh
+++ docker-execution.sh
@@ -8,7 +8,7 @@ docker build -t martinwrangenby/volvo .
 
 # Start the container and run the tests
 RUN_TYPE=$1
-if [ $RUN_TYPE == 'visualRegression' ]; then
+if [ "$RUN_TYPE" == "visualRegression" ]; then
   echo '\n\x1b[32m\x1b[1m\x1b[4mRunning visual-regression tests\x1b[0m'
   docker run --network="host" --name="wdio-test-run-container" -t martinwrangenby/volvo npm run wdio:visual
 else
@@ -20,7 +20,7 @@ fi
 docker cp wdio-test-run-container:app/reports ./
 
 # If we run visual regression suite, copy over baseline images as new baseline screenshots may have been generated during execution
-if [ $RUN_TYPE == 'visualRegression' ]; then
+if [ "$RUN_TYPE" == "visualRegression" ]; then
   docker cp wdio-test-run-container:app/test/utils/visualRegressionBaseline ./test/utils
 fi
 
